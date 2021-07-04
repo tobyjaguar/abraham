a5f2a8fcf9fc77609eaac4cb704645167df530d3
Gene Kogan <kogan.gene@gmail.com>
2021-07-04 19:48:57 -0400

    using eden backend

--- server.py
+++ server.py
@@ -6,98 +6,30 @@ import random
 import pathlib
 import logging
 from datetime import datetime
-
 from flask import Flask, request, render_template, send_from_directory
 from flask import session, flash, redirect, url_for, jsonify
-from celery import Celery
-from celery.utils.log import get_task_logger
-
 import generator
-
+from eden.client import Client
+from eden.datatypes import Image
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
+app.secret_key = 'your_secret_key_here'
 
-# Celery configuration
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+# setup logging
+logging.basicConfig(filename='abraham2.log', 
+                    filemode='a', 
+                    level=logging.DEBUG,
+                    datefmt='%H:%M:%S',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s')
 
-# Initialize Celery
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-
-# Setup logging
-logger = get_task_logger(__name__)
-logger.level = logging.DEBUG
-
-
-@celery.task(bind=True)
-def creation(self, text_input):
-    last_idx = int(sorted(glob.glob('static/results/*'))[-1].split('/')[-1])
-    idx = 1 + last_idx
-    output_dir = 'static/results/%04d'%idx
-
-    image_path = '{}/{}'.format(output_dir, 'image.jpg')
-    config_path = '{}/{}'.format(output_dir, 'config.json')        
-
-    config = {
-        'text_inputs': [{
-            'text': text_input, 
-            'weight': 10.0
-        }],
-        'size': (512, 512),
-        'num_iterations': 1000,
-        'batch_size': 1,
-        'cutn': 24,
-        'weight_decay': 0.1,
-        'learning_rate': 0.1,
-        'lr_decay_after': 400,
-        'lr_decay_rate': 0.995
-    }
-
-    if not os.path.isdir(output_dir):
-        os.mkdir(output_dir)
-
-    with open(config_path, 'w') as outfile:
-        json.dump(config, outfile)
-        
-    def callback(results):
-        #status = 'done' if results['iteration'] == results['num_iterations'] else 'generating'
-        #status = '%s %d %d'%(status, results['iteration'], results['num_iterations'])
-        output = {
-            'current': results['iteration'], 
-            'total': results['num_iterations']
-        }
-        self.update_state(state='PROGRESS', meta=output)
-    
-    img = generator.run(config, callback)
-    img.save(image_path)
-    
-    results = {
-        'current': config['num_iterations'], 
-        'total': config['num_iterations'], 
-        'result': image_path
-    }
-    
-    return results
+# text-to-image client
+client = Client(url = 'http://127.0.0.1:5656', username='abraham', timeout= 990000)
+setup_response = client.setup()
 
 
-@app.route('/getjobs', methods=['GET'])
-def getjobs():
-    logging.info('get jobs')
-    inspect = celery.control.inspect()
-    num_scheduled = len(inspect.scheduled()['celery@G3000'])
-    num_active = len(inspect.active()['celery@G3000'])
-    num_reserved = len(inspect.reserved()['celery@G3000'])
-    jobs = {'scheduled': num_scheduled, 
-            'active': num_active, 
-            'reserved': num_reserved}
-    logging.info(jobs)
-    return jsonify(jobs)
-
-
-@app.route('/getimages', methods=['GET'])
-def getimages():
+@app.route('/get_images', methods=['GET'])
+def get_images():
     creations = []
     results = glob.glob('static/results/*')
     for result in results:
@@ -107,9 +39,18 @@ def getimages():
             continue
         with open(config_path) as f:
             config = json.load(f)
+            
+        # quick hack because of backdated images with different config schema
+        if 'text_inputs' in config:
+            text_input = config['text_inputs'][0]['text']
+        elif 'prompt' in config:
+            text_input = config['prompt']
+        else:
+            text_input = '___UNDEFINED!!!___'
+            
         creation = {
             'image': image_path, 
-            'text_input': config['text_inputs'][0]['text'], 
+            'text_input': text_input, 
             'time': datetime.fromtimestamp(pathlib.Path(image_path).stat().st_mtime)
         }
         creations.append(creation)
@@ -117,54 +58,61 @@ def getimages():
     return jsonify(creations)
     
 
-
-@app.route('/start_creation', methods=['POST'])
-def start_creation():
+@app.route('/request_creation', methods=['POST'])
+def request_creation():
     text_input = request.form['text_input']
-    task = creation.apply_async(args=[text_input])
-    return jsonify({}), 202, {
-        'Location': url_for('taskstatus', task_id=task.id)
+    
+    config = {
+        'prompt': text_input,
+        'width': 512,
+        'height': 512,
+        'iters': 1100,
+        'weight_decay': 0.1,
+        'learning_rate': 0.1,
+        'lr_decay_after': 400,
+        'lr_decay_rate': 0.995
     }
+    response = client.run(config)
 
+    print(response)
+    #pil_image = run_response['output']['creation']
+    #pil_image.save('_hi_saved_from_server.png')
+    logging.info(response)
 
-@app.route('/status/<task_id>')
-def taskstatus(task_id):
-    task = creation.AsyncResult(task_id)
-    if task.state == 'PENDING':
-        response = {
-            'state': task.state,
-            'current': 0,
-            'total': 1,
-            'status': 'Pending...'
-        }
-    elif task.state != 'FAILURE':
-        response = {
-            'state': task.state,
-            'current': task.info.get('current', 0),
-            'total': task.info.get('total', 1),
-            'status': task.info.get('status', '')
-        }
-        if 'result' in task.info:
-            response['result'] = task.info['result']
-    else:
-        response = {
-            'state': task.state,
-            'current': 1,
-            'total': 1,
-            'status': str(task.info),  # this is the exception raised
-        }
-    return jsonify(response)
+    task_id = response['token']
 
+    result = {'task_id': task_id}
+    
+    return jsonify({}), 202, result
+
+@app.route('/get_status', methods=['POST'])
+def get_status():
+    job_id = request.form['task_id']
+    status = 'done'
+    
+    results = client.fetch(token=job_id)
+    print(results)
+    logging.info(results)
+    status = results['status']
+    
+    result = {'status': status}
+    return jsonify({}), 202, result
 
 @app.route('/<path:path>')
-def send_js(path):
+def send_static(path):
     return send_from_directory('static', path)
 
-
+# @app.route('/create')
+# def create():
+#     return render_template('create.html')
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'GET':
         return render_template('create.html')
+
+@app.route('/test')
+def test2():
+    return render_template('create.html')
 
 @app.route('/scripture')
 def scripture():
@@ -173,8 +121,6 @@ def scripture():
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 
 if __name__ == '__main__':
